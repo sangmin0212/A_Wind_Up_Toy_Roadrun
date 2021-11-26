a99d44d7e8b0a2a0fc7d27ff2dded0001366bebf
Sangmin Oh <ohsm1226@ajou.ac.kr>
2021-11-26 11:07:33 +0900

    add player battery funcion
    - Player's speed is full in only 10 second when game is start and player will get battery
    - After 10 second, player's speed will be decreased slowly.
    - If player get battery item(=charge() ), player's speed will be recovered.

--- Player/Player.gd
+++ Player/Player.gd
@@ -9,8 +9,19 @@ var speed = 200
 var reflect_angle = 0
 var isturn = false
 var i = 0
-# life
+# battery and life
+var is_battery_work = true
 var life = 3
+var timer
+
+func _ready():
+	# set timer. After 10 second, player's speed will be decreased
+	timer= Timer.new()
+	add_child(timer)
+	timer.connect("timeout",self,"_on_timeout")
+	timer.set_wait_time(10)
+	timer.start()
+
 
 func _physics_process(delta):
 	# player move itself
@@ -19,7 +30,6 @@ func _physics_process(delta):
 
 	# turn the player based on reflect_angle
 	if i < 10 and isturn:
-		print(reflect_angle)
 		rotate(reflect_angle)
 		i += 1
 		if i == 10:
@@ -38,6 +48,20 @@ func _physics_process(delta):
 		move_and_collide(reflect)
 		
 	# speed will be decreased if you want to charge it, eat battery	
-	# 5초 정도는 full charging으로 갈 수 있게
-	if speed > 0:
-		speed -= 0.2
+	if !is_battery_work and speed >= 0:
+		speed -= 1
+		
+func charge():
+	timer.start()
+	is_battery_work = true
+	speed = 200	
+
+func _on_timeout():
+	print("The battery is dead!")
+	is_battery_work = false
+
+#임시 코드 for battery charging
+func _input(event):
+	if Input.is_action_pressed("ui_up"):
+			charge()
+			
