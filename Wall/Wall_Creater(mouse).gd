845b3b8af1611b0c949bd443d46b79652dbd1455
minhominhominho <hoho6428@ajou.ac.kr>
2021-12-12 15:24:43 +0900

    push

--- Wall/Wall_Creater(mouse).gd
+++ Wall/Wall_Creater(mouse).gd
@@ -13,18 +13,37 @@ var isCreated = false
 var startPoint = Vector2()
 var endPoint = Vector2()
 
+var wallCreatingTimer
+var isPossibleToMakeWall = true
+
+func _ready():
+	wallCreatingTimer = create_timer("wallCreatingTimer",1.5)
+
+func create_timer (item_func, item_time) -> Timer:
+	var timer = Timer.new()    
+	add_child (timer)
+	timer.set_wait_time (item_time)
+	timer.connect("timeout", self, item_func)
+	return timer
+	
+func wallCreatingTimer():
+	isPossibleToMakeWall = true
+
 func _input(event):
-	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
-		if  !isClicked  and event.pressed == true:
-			isClicked = true
-			startPoint = get_global_mouse_position()
-			print("left click press")
-		elif isClicked and event.pressed == false:
-			isClicked = false
-			isCreated = false
-			endPoint = get_global_mouse_position()
-			walls[-1].enable_collision()
-			print("left click unpress")
+	if isPossibleToMakeWall:
+		if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
+			if  !isClicked  and event.pressed == true:
+				isClicked = true
+				startPoint = get_global_mouse_position()
+				print("left click press")
+			elif isClicked and event.pressed == false:
+				wallCreatingTimer.start()
+				isPossibleToMakeWall = false
+				isClicked = false
+				isCreated = false
+				endPoint = get_global_mouse_position()
+				walls[-1].enable_collision()
+				print("left click unpress")
 
 func _process(_delta):
 	var tempWall
