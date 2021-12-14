20703b0ff5d870f458679e045fda32c1cca73f1d
minhominhominho <71825731+minhominhominho@users.noreply.github.com>
2021-12-14 16:55:24 +0900

    OPtion

--- Stage/Main Scene.gd
+++ Stage/Main Scene.gd
@@ -1,5 +1,14 @@
 extends Control
 
+var Main
+var Option
+var volumeSize
+var maxV = 5
+var minV = -50
+
+func _ready():
+	Main = $"Main"
+	Option = $"Option"
 
 func _goto_next_level():
 	get_tree().change_scene("res://Stage/Stage1.tscn")
@@ -11,5 +20,22 @@ func _on_Exit_pressed():
 	get_tree().quit()
 
 
-func _on_Start_button_up():
-	pass # Replace with function body.
+func _on_Back_pressed():
+	Option.hide()
+	Main.show()
+
+
+func _on_Option_pressed():
+	Option.show()
+	Main.hide()
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), -10)
+	volumeSize = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master"))
+	volumeSize = (volumeSize - minV)/(maxV-minV) * 100
+	Option.get_node("VolumeSize").value =  volumeSize
+	
+
+func _on_HSlider_value_changed(value):
+	volumeSize = value/100*(maxV-minV) + minV
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), volumeSize)
+	if value == 0:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), -100)
