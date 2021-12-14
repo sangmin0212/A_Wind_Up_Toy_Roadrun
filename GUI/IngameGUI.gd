a2f4a734d1e5bbfa61d77218d70b7725da221857
minhominhominho <71825731+minhominhominho@users.noreply.github.com>
2021-12-14 21:58:47 +0900

    Last

--- GUI/IngameGUI.gd
+++ GUI/IngameGUI.gd
@@ -4,6 +4,8 @@ extends Node
 
 var sceneController
 var player
+var pauseMenu
+var inGameMenu
 var speedProgressBar
 var wallNum = []
 
@@ -11,15 +13,18 @@ func _ready():
 	player = get_parent().get_node("GameManager").get_node("Player")
 	speedProgressBar = get_node("TextureProgress2")
 	sceneController = get_parent().get_node("SceneController")
+	pauseMenu = get_node("PauseMenu")
+	inGameMenu = get_node("IngameMenu")
+	pauseMenu.hide()
 
 # Set wall limit UI
 func setWallLimitUI(num):
 	for i in range(4):
-		wallNum.append(get_node(str(i)))
+		wallNum.append(get_node("IngameMenu").get_node(str(i)))
 	for i in wallNum:
 		i.hide()
 	if num < 10:
-		get_node("Infinite").hide()
+		get_node("IngameMenu").get_node("Infinite").hide()
 		wallNum[num].show()
 
 # Change UI of remain walls
@@ -31,12 +36,16 @@ func changeWallNumSprite(num):
 func _on_PauseButton_toggled(button_pressed):
 	if button_pressed:
 		get_tree().paused = true
+		inGameMenu.hide()
+		pauseMenu.show()
 	else:
 		get_tree().paused = false
+		inGameMenu.show()
+		pauseMenu.hide()
 
 # Set progress bar of wall creating wait time
 func setProgressValue(value):
-	get_node("TextureProgress").value = value
+	get_node("IngameMenu").get_node("TextureProgress").value = value
 
 func _process(delta):
 	var temp =  player.getSpeed()
@@ -46,8 +55,10 @@ func _process(delta):
 		speedProgressBar.value = temp/ 200 * 100
 
 func _on_Home_pressed():
+	get_tree().paused = false
 	sceneController._load_scene("Main Scene")
 
 
 func _on_Retry_pressed():
-	sceneController._load_scene(get_parent().to_string())
+	get_tree().paused = false
+	sceneController._load_scene(get_parent().name)
